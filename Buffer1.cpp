8a0492ef41bcb9faa3ce16b163237744b330e00a
kelkka <viktor_kelkkanen@hotmail.com>
2013-01-23 17:59:45 +0100

    dfffff
    dfgfg

---
+++ Buffer1.cpp
@@ -0,0 +1,11 @@
+#include "Buffer1.h"
+
+
+Buffer1::Buffer1(void)
+{
+}
+
+
+Buffer1::~Buffer1(void)
+{
+}
---
+++ Shader.cpp
@@ -0,0 +1,11 @@
+#include "Shader.h"
+
+
+Shader1::Shader(void)
+{
+}
+
+
+Shader1::~Shader(void)
+{
+}
