a6e131f272377b713b3275cf0fe6302d491eef55
a Krüger <anders.kruger@hotmail.com>
2013-01-23 18:33:36 +0100

    stdafx.h added

---
+++ stdafx.h
@@ -0,0 +1,22 @@
+#ifndef _STDAFX_H_
+#define _STDAFX_H_
+
+#include <windows.h>
+
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10math.h>
+#include <d3dx11Effect.h>
+#include <d3dCompiler.h>
+
+#include <fstream>
+#include <vector>
+
+#pragma comment(lib, "Effects11.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dxerr.lib")
+#pragma comment(lib, "d3dx11.lib")
+#pragma comment(lib, "d3dx10.lib")
+
+#endif
